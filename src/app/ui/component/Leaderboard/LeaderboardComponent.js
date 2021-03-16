1f0e90d33fe64de9a1f56dd3b1a4aa62062cbfb1
Vlad314Dev <vlad314dev@gmail.com>
2021-03-17 00:22:27 +0200

    Remove unused text

--- src/app/ui/component/Leaderboard/LeaderboardComponent.js
+++ src/app/ui/component/Leaderboard/LeaderboardComponent.js
@@ -10,7 +10,6 @@ class LeaderboardComponent extends PureComponent
         return (
             <div block="Leaderboard">
                 <div block="Leaderboard" elem="Title">TOP 10</div>
-                <div block="Leaderboard" elem="UserRank">Authorize to see your current rank</div>
                 <LeaderboardList/>
             </div>
         )
